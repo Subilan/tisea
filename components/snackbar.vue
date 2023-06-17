9d72f8eb0d3582fac58b0e23779c96f581e4ae71
Subilan <suyuanliang1995@gmail.com>
2023-06-17 23:24:10 +0800

    component: snackbar

---
+++ components/snackbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <Transition name="fadeDown">
+        <div v-if="props.modelValue" class="snackbar">
+            <div class="text">
+                <slot></slot>
+            </div>
+        </div>
+    </Transition>
+</template>
+
+<script setup>
+const props = defineProps(['modelValue']);
+defineEmits(['update:modelValue']);
+</script>
+
+<style lang="less">
+.snackbar {
+    display: flex;
+    align-items: center;
+    background: #212121;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, .2);
+    height: 30px;
+    font-size: 18px;
+    position: fixed;
+    top: 0;
+    color: white;
+    padding: 10px 20px;
+    font-family: Consolas, ui-monospace, '微软雅黑', 'Microsoft Yahei', 'Courier New', Courier, monospace;
+}
+
+.text {
+    animation: OpacityFade cubic-bezier(.8, 0, 0, 1) 1s;
+}
+</style>
