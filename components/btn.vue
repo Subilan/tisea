6683bd5c571a882817f6ec079d4f92d1023817a2
Subilan <suyuanliang1995@gmail.com>
2023-06-17 23:24:00 +0800

    component: btn

---
+++ components/btn.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="button">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+.button {
+    padding: 8px 16px;
+    border: 1px solid rgba(0, 0, 0, .2);
+    display: inline-block;
+    border-radius: 5px;
+    cursor: pointer;
+    background: white;
+    color: #2196f3;
+    transition: all .2s ease;
+}
+
+.button.primary {
+    background: #2196f3;
+    color: white;
+    border: none;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, .2);
+}
+
+.button[disabled] {
+    cursor: not-allowed;
+    box-shadow: none;
+    border: none;
+    background: gray;
+    color: white;
+}
+
+.button:hover {
+    opacity: .8;
+    
+}
+</style>
