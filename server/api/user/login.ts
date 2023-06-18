57e71887d7536e1ed303d000614ea279f04c77f5
Subilan <suyuanliang1995@gmail.com>
2023-06-18 15:09:44 +0800

    refactor login

--- server/api/user/login.ts
+++ server/api/user/login.ts
@@ -5,29 +5,22 @@ export default defineEventHandler(async e => {
 	const body = await readBody(e);
 	const type = body.loginType as 'oasis' | 'key';
 	if (type === 'key') {
-		let k = body.key.split('~');
-		const token = k[1];
-		const playername = k[0];
-		return {
-			status: await Auth.verifyToken(playername, token),
-			code: 200
-		};
+		const key = body.key;
+		if (!key) return er(ERR.NOT_ENOUGH_ARGUMENT);
+		try {
+			if ((await Auth.verifyKey(key)) === true) return ok();
+			else return er(ERR.VERIFICATION);
+		} catch (e: any) {
+			return er(e.message);
+		}
 	} else {
 		const username = body.username;
 		const password = body.password;
 		try {
-			const r = await Auth.loginOasis(username, password)
-			return {
-				status: true,
-				code: 200,
-                msg: Func.encrypt(`${username}.${password}`).toString()
-			};
+			if (await Auth.loginOasis(username, password)) return ok(Func.encrypt(`${username}.${password}`).toString());
+			else return er(ERR.VERIFICATION);
 		} catch (e: any) {
-			return {
-				status: false,
-				code: 403,
-                msg: e.message
-			};
+			return er(e.message);
 		}
 	}
 });
