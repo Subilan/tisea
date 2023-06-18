b5c117c52abb44293af93e92bf00c19e0fea9816
Subilan <suyuanliang1995@gmail.com>
2023-06-18 22:21:50 +0800

    feat: create user on first login

--- server/api/user/login.ts
+++ server/api/user/login.ts
@@ -2,6 +2,45 @@ import Auth from '../../utils/static/Auth';
 import DB from '../../utils/static/DB';
 import Func from '../../utils/static/Func';
 
+async function initializeUser(username: string) {
+	const user = await DB.getUser({
+		username
+	});
+	if (user !== null) {
+		DB.upsertUser({
+			username
+		}, {
+			latestConnect: new Date().toString()
+		})
+		return;
+	}
+	const binding = await DB.getBinding({
+		username
+	});
+	// default values
+	let uid = Func.genStr(10);
+	let playername = '';
+	let displayname = username;
+	let group: UserGroup = 'player';
+
+	if (binding !== null) {
+		playername = binding.playername;
+	}
+
+	DB.upsertUser(
+		{
+			uid
+		},
+		{
+			username,
+			displayname,
+			playername,
+			group,
+			latestConnect: new Date().toString()
+		}
+	);
+}
+
 export default defineEventHandler(async e => {
 	const body = await readBody(e);
 	const type = body.loginType as 'oasis' | 'key';
@@ -19,22 +58,7 @@ export default defineEventHandler(async e => {
 		const password = body.password;
 		try {
 			if (await Auth.loginOasis(username, password)) {
-				const user = await DB.getUser({
-					username
-				});
-				let uid = Func.genStr(10);
-
-				if (user !== null) {
-					uid = user.uid as string;
-				}
-
-				DB.upsertUser(uid, {
-					username,
-					displayname: username,
-					playername: '',
-					group: 'player',
-					latestConnect: new Date().toString()
-				});
+				await initializeUser(username);
 				return ok(Func.encrypt(`${username}.${password}`).toString());
 			} else return er(ERR.VERIFICATION);
 		} catch (e: any) {
