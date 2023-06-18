ceabf100f980fe535c97a92fc8738abe8e6f2e20
Subilan <suyuanliang1995@gmail.com>
2023-06-18 11:32:12 +0800

    feat(api): verify

--- server/api/user/verify.ts
+++ server/api/user/verify.ts
@@ -1,3 +1,47 @@
-export default defineEventHandler(e => {
-    
-})
+import Auth from '../../utils/auth';
+import { ERR, er, ok } from '../../utils/common';
+import Func from '../../utils/func';
+
+export default defineEventHandler(async e => {
+	const q = getQuery(e);
+	if (q.method === 'oasis') {
+		if (q.data === null) {
+			return er(ERR.NOT_ENOUGH_ARGUMENT);
+		}
+		let username,
+			password,
+			lg = null;
+		try {
+			let de = Func.decrypt(q.data as string).split('.');
+			username = de[0];
+			password = de[1];
+		} catch (err) {
+			return er(ERR.CIPHER_DECRYPTION);
+		}
+		try {
+			lg = await Auth.loginOasis(username, password);
+			if (lg.status.code === 'ok') return ok();
+			else return er(ERR.INTERNAL_REQUEST_FAILED);
+		} catch (err: any) {
+			return er(err.message);
+		}
+	} else if (q.method === 'key') {
+		if (q.key === null) {
+			return er(ERR.NOT_ENOUGH_ARGUMENT);
+		}
+
+		let playername,
+			token = null;
+		let k = (q.key as string).split('~');
+		playername = k[0];
+		token = k[1];
+		const r = await Auth.verifyToken(playername, token);
+		if (r === true) {
+			return ok();
+		} else {
+			return er();
+		}
+	} else {
+		return er(ERR.INVALID_REQUEST);
+	}
+});
