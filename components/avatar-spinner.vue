a1e1a38c6deda6ddf425cdf95af8e834bf53ef41
Subilan <suyuanliang1995@gmail.com>
2023-06-20 23:58:38 +0800

    chore: import spinner code

---
+++ components/avatar-spinner.vue
@@ -0,0 +1,68 @@
+<template>
+    <div v-bind:style="styles" class="spinner spinner--circle-5"></div>
+</template>
+<script>
+export default {
+    props: {
+        size: {
+            default: '36px'
+        }
+    },
+    computed: {
+        styles() {
+            return {
+                width: this.size,
+                height: this.size
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.spinner {
+    transform: translate3d(0, 0, 0);
+    position: relative;
+    background: #00bcd4;
+    border-radius: 50%;
+    overflow: hidden;
+}
+
+.spinner:before,
+.spinner:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+}
+
+.spinner:before {
+    animation: circle-5-spinner 3s infinite;
+    transform-origin: 0 50%;
+    left: 0;
+    background: #b2ebf2;
+}
+
+.spinner:after {
+    animation: circle-5-spinner 3s 0.2s reverse infinite;
+    transform-origin: 100% 50%;
+    right: 0;
+    background: #e0f7fa;
+}
+
+@keyframes circle-5-spinner {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    50% {
+        transform: rotate(360deg);
+    }
+
+    100% {
+        transform: rotate(0deg);
+    }
+}
+</style>
---
+++ components/spinner.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :style="styles" class="spinner spinner--circle-2"></div>
+</template>
+<script>
+export default {
+    props: {
+        size: {
+            default: '40px'
+        },
+        background: {
+            default: '#e0f7fa'
+        },
+        color: {
+            default: '#00bcd4'
+        },
+        stroke: {
+            default: '3px'
+        }
+    },
+    computed: {
+        styles() {
+            return {
+                width: this.size,
+                height: this.size,
+                border: this.stroke + ' solid ' + this.background,
+                borderTopColor: this.color
+            }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.spinner {
+    box-sizing: border-box;
+    border-radius: 100%;
+    border: 10px solid rgba(255, 255, 255, 0.2);
+    animation: circle-2-spin 1s infinite linear;
+}
+
+@keyframes circle-2-spin {
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
