893cfa014d4da258ac212ca0bcf42b9bbd1a64a4
Subilan <suyuanliang1995@gmail.com>
2023-07-03 22:18:11 +0800

    chore: exception handling

--- server/api/updateOnlineState.ts
+++ server/api/updateOnlineState.ts
@@ -1,6 +1,7 @@
 import {ng, ok} from "../utils/response";
 import {User} from "../utils/classes/User";
 
+// using navigator.sendBeacon(url, data: string) to send.
 export default defineEventHandler(async e => {
     const body = await readBody(e);
     let json: Partial<IUser> = {}
@@ -10,10 +11,14 @@ export default defineEventHandler(async e => {
         return ng("Invalid JSON object.")
     }
     if (json.id === undefined || json.isOnline === undefined) {
-        return ng(ERR.INVALID_PARAMETER);
+        return ng(ERR.INVALID_ARGUMENT);
     }
     const id = json.id;
     const isOnline = json.isOnline;
-    await (await User.build(id)).setOnline(isOnline)
+    try {
+        await (await User.build(id)).setOnline(isOnline)
+    } catch (e: any) {
+        return ng(e.message);
+    }
     return ok();
 })
