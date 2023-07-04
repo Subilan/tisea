a88c71625c3e8f578986a3719eded97f0c5d5f20
Subilan <suyuanliang1995@gmail.com>
2023-07-04 19:04:06 +0800

    refactor: direct switch

--- server/api/index.ts
+++ server/api/index.ts
@@ -11,44 +11,39 @@ export default defineEventHandler(async e => {
     const params = body.params as dict<any>;
 
     try {
-        if (action.startsWith("user.")) {
-            switch (action) {
-                case "user.create": {
-                    const creation = await Creation.build({
-                        displayname: requireNonEmpty(params.displayname),
-                        minecraft: requireNonEmpty(params.minecraft),
-                        password: requireNonEmpty(params.password),
-                        oasis: params.oasis ?? null
-                    })
-                    await creation.create()
-                    return ok();
-                }
-
-                case "user.alter": {
-                    const id = requireNonEmpty(params.id);
-                    const user = await User.build(id);
-                    await user.alter(params.set)
-                    return ok();
-                }
+        switch (action) {
+            case "user.create": {
+                const creation = await Creation.build({
+                    displayname: requireNonEmpty(params.displayname),
+                    minecraft: requireNonEmpty(params.minecraft),
+                    password: requireNonEmpty(params.password),
+                    oasis: params.oasis ?? null
+                })
+                await creation.create()
+                return ok();
             }
-        }
 
-        if (action.startsWith('auth.')) {
-            switch (action) {
-                case "auth.login": {
-                    const displayname = requireNonEmpty(params.displayname);
-                    const pwd = requireNonEmpty(params.password);
-                    const user = await User.build("", displayname);
-                    await user.login(pwd);
-                    return ok(user.toToken());
-                }
+            case "user.alter": {
+                const id = requireNonEmpty(params.id);
+                const user = await User.build(id);
+                await user.alter(params.set)
+                return ok();
+            }
 
-                case "auth.logout": {
-                    const id = requireNonEmpty(params.id);
-                    const user = await User.build(id);
-                    await user.logout();
-                    return ok();
-                }
+
+            case "auth.login": {
+                const displayname = requireNonEmpty(params.displayname);
+                const pwd = requireNonEmpty(params.password);
+                const user = await User.build("", displayname);
+                await user.login(pwd);
+                return ok(user.toToken());
+            }
+
+            case "auth.logout": {
+                const id = requireNonEmpty(params.id);
+                const user = await User.build(id);
+                await user.logout();
+                return ok();
             }
         }
 
