e8cf6c17ab1267fd9d6258d9bc23fd66e489af58
Subilan <suyuanliang1995@gmail.com>
2023-07-04 19:02:29 +0800

    delete old apis

--- server/api-old/user/actions/refresh-latest-connect.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async e => {
-	const body = await readBody(e);
-    const username = body.username;
-    if (!username) {
-        return er(ERR.NOT_ENOUGH_ARGUMENT);
-    }
-    await Func.refreshLatestConnect(username);
-});
