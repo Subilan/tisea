105c0c86bfdb4950b7a2bdb2795833e2cb961cb2
Subilan <suyuanliang1995@gmail.com>
2023-07-05 16:25:17 +0800

    feat: custom `<a>` alt

---
+++ components/r.vue
@@ -0,0 +1,27 @@
+<template>
+  <a class="r" :href="props.href" :target="newTab ? '_blank' : ''">
+    <slot/>
+    <span class="mdi mdi-arrow-top-right"/> </a>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  href: {
+    type: String
+  },
+  newTab: {
+    type: Boolean,
+    default: true
+  }
+})
+</script>
+<style scoped lang="less">
+.r {
+  text-decoration: underline dotted 1px;
+  opacity: .8;
+  color: #00e3db;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
