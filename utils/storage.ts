3d2d9a9ed0088d012dbf66ec9dad57d6abf5566e
Subilan <suyuanliang1995@gmail.com>
2023-07-05 11:51:10 +0800

    fix: inner storage set

--- utils/storage.ts
+++ utils/storage.ts
@@ -5,12 +5,31 @@ const KEYS = {
 }
 
 export default class Storage {
+    public static set(key: string, value: any) {
+        if (process.client) {
+            localStorage.setItem(key, value);
+        }
+    }
+
+    public static get(key: string) {
+        if (process.client) {
+            return localStorage.getItem(key)
+        }
+        return null;
+    }
+
+    public static rm(key: string) {
+        if (process.client) {
+            localStorage.removeItem(key)
+        }
+    }
+
     public static get token(): Nullable<string> {
-        return localStorage.getItem(KEYS.user.token);
+        return this.get(KEYS.user.token);
     }
 
     public static set token(value) {
-        if (value === null) localStorage.removeItem(KEYS.user.token);
-        else localStorage.setItem(KEYS.user.token, value);
+        if (value === null) this.rm(KEYS.user.token);
+        else this.set(KEYS.user.token, value);
     }
 }
