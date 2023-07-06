b1c6830a27ece4d8069afaa1045fc9afa18eb241
Subilan <suyuanliang1995@gmail.com>
2023-07-06 15:57:17 +0800

    feat: api for value checking in backend

---
+++ server/api/common/check-value.ts
@@ -0,0 +1,40 @@
+import {getUUIDFromName, requireNonEmpty} from "../../utils/common";
+
+export default defineEventHandler(async e => {
+    const body = await readBody(e);
+    try {
+        const type: CheckTypes = requireNonEmpty(body.type);
+        const value = requireNonEmpty(body.value);
+        switch (type) {
+            case "user.minecraft.valid": {
+                const login = await getUUIDFromName(value);
+                return ok(null, login !== null)
+            }
+
+            case "user.username.uniqueness": {
+                const r = await UserUtil.doesExist({username: value});
+                return ok(null, !r);
+            }
+
+            case "user.minecraft.uniqueness": {
+                const r = await UserUtil.get({
+                    minecraft: value
+                });
+                return ok(null, r === null);
+            }
+
+            case "user.oasis.uniqueness": {
+                const r = await UserUtil.get({
+                    oasisUsername: value
+                });
+                return ok(null, r === null);
+            }
+
+            default: {
+                return ng(ERR.UNSUPPORTED_OPERATION);
+            }
+        }
+    } catch (e: any) {
+        return ng(e.message);
+    }
+})
