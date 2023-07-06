997c86c3d3657b48d0d03762a148001757dfe281
Subilan <suyuanliang1995@gmail.com>
2023-07-06 17:32:21 +0800

    fix: invalid beacon log

--- server/api/common/user/set-online-status.ts
+++ server/api/common/user/set-online-status.ts
@@ -1,25 +1,19 @@
 import {User} from "../../../utils/classes/User";
-import {requireNonEmpty} from "../../../utils/common";
 
 // important: please use navigator.sendBeacon(url, data: string) to test such API, absolutely not other API tools.
 export default defineEventHandler(async e => {
     const body = await readBody(e);
     let token = '';
     let isOnline = false;
-    try {
-        token = requireNonEmpty(body.token);
-        isOnline = requireNonEmpty(body.isOnline);
-    } catch (e: any) {
-        console.warn(`Beacon warning: ${e.message}`);
-        return null;
-    }
+    token = body.token;
+    isOnline = body.isOnline;
     try {
         const user = await User.fromToken(token);
         await user.setOnline(isOnline);
         console.log(`Set isOnline to ${isOnline} for user ${user.username}.`);
         return null;
     } catch (e: any) {
-        console.warn(`Beacon warning: ${e.message}`);
+        if (e.message !== ERR.TOKEN.INVALID) console.warn(`Beacon warning: ${e.message}`);
         return null;
     }
 })
