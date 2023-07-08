ecb28c88083d9e5b593526adaf17186fdef563e4
Subilan <suyuanliang1995@gmail.com>
2023-07-08 22:51:32 +0800

    feat: <mobile-only/>

---
+++ components/mobile-only.vue
@@ -0,0 +1,14 @@
+<template>
+  <slot v-if="mobile()"/>
+</template>
+
+<script setup lang="ts">
+import isMobile from "ismobilejs";
+function mobile() {
+  return window.innerWidth < 900 || isMobile(navigator.userAgent).any;
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
