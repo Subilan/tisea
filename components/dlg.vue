88327905f23b5777b6c050d7243901a1567b7870
Subilan <suyuanliang1995@gmail.com>
2023-07-11 11:30:08 +0800

    chore: overflow

--- components/dlg.vue
+++ components/dlg.vue
@@ -104,7 +104,7 @@ const emit = defineEmits(['update:modelValue'])
       line-height: 1.5;
       font-size: 18px;
       max-height: 700px;
-      overflow: visible;
+      overflow-y: auto;
     }
 
     .dialog-card-actions {
