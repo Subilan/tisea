61dc8f825a4b5c04e730baa7c00e082b6a0de68a
Subilan <suyuanliang1995@gmail.com>
2023-11-28 22:39:27 +0800

    reload - old components will be added later

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// @ts-nocheck
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	app: {
-		head: {
-			charset: 'utf-8',
-			viewport: 'width=device-width, initial-scale=1',
-			title: 'Tisea - Anything Titanium.',
-			link: [
-				{
-					rel: 'preconnect',
-					href: 'https://google-fonts.mirrors.sjtug.sjtu.edu.cn'
-				},
-				{
-					rel: 'stylesheet',
-					href: 'https://google-fonts.mirrors.sjtug.sjtu.edu.cn/css2?family=Rubik:ital,wght@0,400;0,500;0,700;1,400&display=swap'
-				}
-			]
-		},
-		pageTransition: {
-			name: 'default-fade',
-			mode: 'out-in'
-		}
-	},
-	css: ['@/assets/styles/main.less'],
-	routeRules: {
-		'/auth': { ssr: false }
-	},
-	plugins: [{
-		src: '@/plugins/routerSettings.ts',
-		mode: 'client'
-	}, {
-		src: '@/plugins/mitt.ts',
-		mode: 'client'
-	}, {
-		src: '@/plugins/cookies.ts',
-		mode: 'client'
-	}]
-});
+  devtools: { enabled: true }
+})
--- pages/terms.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        terms
-    </div>
-</template>
